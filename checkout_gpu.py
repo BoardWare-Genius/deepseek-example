aa96fa060f4da0cff18d30a3ba3617c121f9876e
chenyunda218 <chenyunda218@gmail.com>
2025-02-21 17:25:57 +0800

    fixup

--- checkout_gpu.py
+++ checkout_gpu.py
@@ -3,11 +3,14 @@ import uvicorn
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse, StreamingResponse
 
+print(f"version:{torch.__version__}, cuda:{torch.cuda.is_available()}")
+
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
+
     return f"version:{torch.__version__}, cuda:{torch.cuda.is_available()}"
 
 if __name__ == "__main__":
