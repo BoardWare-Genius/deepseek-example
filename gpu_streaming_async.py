cb33d6e3e28dde6e3a52237fd7c697eedfe77883
chenyunda218 <chenyunda218@gmail.com>
2025-02-21 09:20:23 +0800

    fixup

--- gpu_streaming_async.py
+++ gpu_streaming_async.py
@@ -1,4 +1,5 @@
 import asyncio
+import contextvars
 import time
 
 import torch
@@ -14,51 +15,42 @@ tokenizer = AutoTokenizer.from_pretrained(model_path)
 model = AutoModelForCausalLM.from_pretrained(
     model_path).to(device)  # 将模型移动到 GPU
 
+canceled_context = contextvars.ContextVar("canceled", default=False)
 
-def generate_streaming_yield(prompt, max_length=100):
+
+async def generate_streaming_text(prompt, callback, max_length=100):
+    if callback is None:
+        return
     inputs = tokenizer(prompt, return_tensors="pt").to(device)  # 将输入数据移动到 GPU
     input_ids = inputs["input_ids"]
 
+    # 初始化生成的文本
+    generated_text = prompt
+    loop = asyncio.get_running_loop()
     with torch.no_grad():
         for _ in range(max_length):
-            # print(time.time())
-            outputs = model(input_ids)
-            # 获取下一个词的logits
+            outputs = await loop.run_in_executor(None, model, input_ids)
             next_token_logits = outputs.logits[:, -1, :]
-
-            # 选择概率最高的词
             next_token_id = torch.argmax(
                 next_token_logits, dim=-1).unsqueeze(0)
-
-            # 将生成的词添加到输入中
             input_ids = torch.cat([input_ids, next_token_id], dim=-1)
-
-            # 解码生成的词
             next_token = tokenizer.decode(
                 next_token_id[0], skip_special_tokens=True)
 
-            # 更新生成的文本
-            yield next_token
-
-            # 如果生成结束符，则停止生成
+            callback(next_token)
+            generated_text += next_token
             if next_token_id == tokenizer.eos_token_id:
                 break
 
+    return generated_text
 
-async def generate_streaming_text(prompt, max_length=100):
-    generated_text = prompt
-    gen = generate_streaming_yield(prompt, max_length)
-    for _ in range(max_length):
-        text = next(gen)
-        if text == tokenizer.eos_token_id:
-            break
-        print(text, end="", flush=True)
-        generated_text += text
-        await asyncio.sleep(0)
+
+def print_callback(text):
+    print(text, end="", flush=True)
 
 
 async def main():
-    await asyncio.gather(generate_streaming_text("你是誰"), generate_streaming_text("who are you?"))
+    await asyncio.gather(generate_streaming_text("你是誰", callback=print_callback))
 
 # 示例调用
 prompt = "who are you?"
